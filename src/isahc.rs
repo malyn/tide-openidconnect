5f3306c3926d6aaa253934d7bcfb43bb651db0b9
Michael Alyn Miller <malyn@strangeGizmo.com>
2021-05-22 11:58:58 -0700

    Add proper error handling to the Isaahc HTTP client wrapper

--- src/isahc.rs
+++ src/isahc.rs
@@ -1,8 +1,27 @@
+// TODO Move this into `lib.rs` so that we protect the entire library?
+#![deny(clippy::unwrap_in_result, clippy::unwrap_used)]
+
 use bytes::BufMut;
 use futures_lite::io::AsyncReadExt;
 use isahc::{config::RedirectPolicy, prelude::*, HttpClient, Request};
 use once_cell::sync::Lazy;
 use openidconnect::{HttpRequest, HttpResponse};
+
+///
+/// Error type returned by failed Isahc HTTP requests.
+///
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum Error {
+    /// Error returned by Isahc crate.
+    #[error("Isahc request failed")]
+    Isahc(#[source] isahc::Error),
+    /// Non-Isahc HTTP error.
+    #[error("HTTP error")]
+    Http(#[source] http::Error),
+    /// I/O error.
+    #[error("I/O error")]
+    Io(#[source] std::io::Error),
+}
 
 // Isahc recommends that you create a single client per "area of application"
 // and reuse that client through your code. We have to create a global client
@@ -14,30 +33,34 @@ static HTTP_CLIENT: Lazy<HttpClient> = Lazy::new(|| {
     HttpClient::builder()
         .redirect_policy(RedirectPolicy::None)
         .build()
-        .unwrap()
+        .expect("Unable to initialize Isahc client.")
 });
 
-pub(crate) async fn http_client(request: HttpRequest) -> Result<HttpResponse, isahc::Error> {
+pub(crate) async fn http_client(openid_request: HttpRequest) -> Result<HttpResponse, Error> {
     let mut request_builder = Request::builder()
-        .method(request.method)
-        .uri(request.url.as_str());
-    for (name, value) in &request.headers {
+        .method(openid_request.method)
+        .uri(openid_request.url.as_str());
+    for (name, value) in &openid_request.headers {
         request_builder = request_builder.header(name.as_str(), value.as_bytes());
     }
-    let isahc_request = request_builder.body(request.body).unwrap();
+    let request = request_builder
+        .body(openid_request.body)
+        .map_err(Error::Http)?;
 
-    let isahc_response = HTTP_CLIENT.send_async(isahc_request).await?;
-    let status_code = isahc_response.status();
-    let headers = isahc_response.headers().to_owned();
-    let mut response_body = isahc_response.into_body();
+    let response = HTTP_CLIENT
+        .send_async(request)
+        .await
+        .map_err(Error::Isahc)?;
+    let status_code = response.status();
+    let headers = response.headers().to_owned();
+    let mut response_body = response.into_body();
 
     let mut body = vec![];
     let mut buf = [0u8; 1024];
     loop {
-        match response_body.read(&mut buf[..]).await {
-            Ok(0) => break,
-            Ok(len) => body.put(&buf[..len]),
-            Err(err) => return Err(err.into()),
+        match response_body.read(&mut buf[..]).await.map_err(Error::Io)? {
+            0 => break,
+            len => body.put(&buf[..len]),
         }
     }
 
